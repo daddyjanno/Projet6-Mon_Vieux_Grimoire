5481e801cf5db005865b2998d327f1b9309b8f3f
daddyjanno <jn.drugmand@gmail.com>
2025-08-29 18:25:19 +0200

    new post and get routes

--- back/app.js
+++ back/app.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 
+const Book = require('./models/Book.js')
+
 const app = express()
 
 mongoose
@@ -28,35 +30,26 @@ app.use((req, res, next) => {
     next()
 })
 
-app.post('', (req, res, next) => {
-    console.log(req.body)
-    res.status(201).json({
-        message: 'Objet créé !',
+app.post('api/books', (req, res, next) => {
+    delete req.body._id
+    const book = new Book({
+        ...req.body,
     })
+    book.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré' }))
+        .catch((error) => res.status(400).json({ error }))
 })
 
-app.get('', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl:
-                'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl:
-                'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ]
-    res.status(200).json(stuff)
+app.get('api/books/:id', (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+        .then((book) => res.status(200).json(book))
+        .catch((error) => res.status(404).json({ error }))
+})
+
+app.get('api/books', (req, res, next) => {
+    Book.find()
+        .then((books) => res.status(200).json(books))
+        .catch((error) => res.status(400).json({ error }))
 })
 
 module.exports = app
