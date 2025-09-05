216b0c3a4c59688afe3cbdb4ee5c7617fe129d5f
daddyjanno <jn.drugmand@gmail.com>
2025-09-05 17:11:52 +0200

    create book routes

--- back/app.js
+++ back/app.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 
-const Book = require('./models/Book.js')
-
 const app = express()
+
+const bookRoutes = require('./routes/book')
 
 mongoose
     .connect(
@@ -30,38 +30,8 @@ app.use((req, res, next) => {
     next()
 })
 
-app.get('/api/books', (req, res, next) => {
-    Book.find()
-        .then((books) => res.status(200).json(books))
-        .catch((error) => res.status(400).json({ error }))
-})
+app.use(bodyParse.json())
 
-app.get('/api/books/:id', (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
-        .then((book) => res.status(200).json(book))
-        .catch((error) => res.status(404).json({ error }))
-})
-
-app.post('/api/books', (req, res, next) => {
-    delete req.body._id
-    const book = new Book({
-        ...req.body,
-    })
-    book.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré' }))
-        .catch((error) => res.status(400).json({ error }))
-})
-
-app.put('/api/books/:id', (req, res, next) => {
-    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié' }))
-        .catch((error) => res.status(400).json({ error }))
-})
-
-app.delete('/api/books/:id', (req, res, next) => {
-    Book.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé' }))
-        .catch((error) => res.status(400).json({ error }))
-})
+app.use('/api/books', bookRoutes)
 
 module.exports = app
