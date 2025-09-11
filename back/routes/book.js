f7e7cd607b66e2033180ab4962eb0cf6366356de
daddyjanno <jn.drugmand@gmail.com>
2025-09-11 07:35:59 +0200

    update book routes

--- back/routes/book.js
+++ back/routes/book.js
@@ -1,40 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+const bookControl = require('../controllers/book')
+
 const Book = require('../models/Book')
 
-router.get('/', (req, res, next) => {
-    Book.find()
-        .then((books) => res.status(200).json(books))
-        .catch((error) => res.status(400).json({ error }))
-})
+router.get('/', bookControl.getAllBooks())
 
-router.get('/:id', (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
-        .then((book) => res.status(200).json(book))
-        .catch((error) => res.status(404).json({ error }))
-})
+router.get('/:id', bookControl.findOneBook())
 
-router.post('/', (req, res, next) => {
-    delete req.body._id
-    const book = new Book({
-        ...req.body,
-    })
-    book.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré' }))
-        .catch((error) => res.status(400).json({ error }))
-})
+router.post('/', bookControl.createBook())
 
-router.put('/:id', (req, res, next) => {
-    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié' }))
-        .catch((error) => res.status(400).json({ error }))
-})
+router.put('/:id', bookControl.modifyBook())
 
-router.delete('/:id', (req, res, next) => {
-    Book.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé' }))
-        .catch((error) => res.status(400).json({ error }))
-})
+router.delete('/:id', bookControl.deleteBook())
 
 module.exports = router
